e1b37b7cf3676adeebf3a3958037b09a34f75eda
yabbas2 <yousef.abbas.2@outlook.com>
2026-05-18 05:25:53 -0500

    Add assignment 5 part 1 solution

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,5 +1,8 @@
 #include <arpa/inet.h>
+#include <fcntl.h>
 #include <netdb.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,34 +13,29 @@
 
 #define LISTEN_BACKLOG 10
 #define TMP_FILE_PATH "/var/tmp/aesdsocketdata"
-#define RECV_DATA_MAX_LEN 5000
+#define RECV_DATA_MAX_LEN 1024
 
-static int recvCompletePacket(int client_fd, char *recv_buffer, ssize_t bytes_left) {
-    ssize_t bytes_read;
+static bool running = true;
 
-    do {
-        if (bytes_left <= 0) {
-            return -1; // no enough space to receive more data
-        }
-
-        bytes_read = recv(client_fd, recv_buffer, bytes_left, 0);
-        if (bytes_read == 0) {
-            return 0; // connection closed
-        } else if (bytes_read < 0) {
-            perror("recv");
-            return -1;
-        } else {
-            recv_buffer += bytes_read;
-            bytes_left -= bytes_read;
-            printf("aesdsocket: received %d bytes\n", (int)bytes_read);
-        }
-
-    } while (recv_buffer[bytes_read - 1] != '\n');
-
-    return bytes_read;
+static void signal_handler(int signum) {
+    printf("Received signal %d\n", signum);
+    running = false;
 }
 
 int main(int argc, char *argv[]) {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = signal_handler;
+    sigfillset(&sa.sa_mask);
+    if (sigaction(SIGINT, &sa, NULL) < 0) {
+        perror("sigaction");
+        return -1;
+    }
+    if (sigaction(SIGTERM, &sa, NULL) < 0) {
+        perror("sigaction");
+        return -1;
+    }
+
     struct addrinfo hints;
     struct addrinfo *result;
     int socket_fd;
@@ -60,13 +58,17 @@ int main(int argc, char *argv[]) {
             perror("socket");
             continue;
         }
-
+        int opt = 1;
+        if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+            perror("setsockopt");
+            close(socket_fd);
+            continue;
+        }
         if (bind(socket_fd, p->ai_addr, p->ai_addrlen) != 0) {
             perror("bind");
             close(socket_fd);
             continue;
         }
-
         break;
     }
 
@@ -77,21 +79,48 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
+    bool is_daemon = false;
+    if (argc > 1) {
+        if (strcmp(argv[1], "-d") == 0) {
+            is_daemon = true;
+        }
+    }
+
+    if (is_daemon) {
+        if (daemon(0, 0) != 0) {
+            perror("daemon");
+            return -1;
+        }
+    }
+
     if (listen(socket_fd, LISTEN_BACKLOG) != 0) {
         perror("listen");
         close(socket_fd);
         return -1;
     }
 
+    char *buffer = malloc(RECV_DATA_MAX_LEN);
+    if (buffer == NULL) {
+        perror("malloc");
+        return -1;
+    }
+
+    int f_fd = open(TMP_FILE_PATH, O_APPEND | O_CREAT | O_RDWR, 0644);
+    if (f_fd < 0) {
+        perror("open");
+        return -1;
+    }
+
     printf("aesdsocket: listening on port 9000\n");
     openlog("aesdsocket", 0, LOG_USER);
 
-    while (1) {
+    while (running) {
         struct sockaddr client_addr;
         socklen_t client_addr_len = sizeof(struct sockaddr);
         int client_fd = accept(socket_fd, &client_addr, &client_addr_len);
         if (client_fd < 0) {
             perror("accept");
+            close(client_fd);
             continue;
         }
 
@@ -101,51 +130,76 @@ int main(int argc, char *argv[]) {
         }
         printf("aesdsocket: accepted connection from %s\n", client_ip);
 
-        char *recv_buffer = malloc(RECV_DATA_MAX_LEN);
-        if (recv_buffer == NULL) {
-            perror("malloc");
-            close(client_fd);
-            continue;
-        }
+        ssize_t bytes_read;
+        bool complete_packet = false;
+        do {
+            bytes_read = recv(client_fd, buffer, RECV_DATA_MAX_LEN, 0);
+            if (bytes_read == 0) {
+                break;
+            } else if (bytes_read < 0) {
+                perror("recv");
+                break;
+            } else {
+                printf("aesdsocket: received %d bytes\n", (int)bytes_read);
+            }
 
-        ssize_t bytes_left = RECV_DATA_MAX_LEN;
-        ssize_t bytes_read = recvCompletePacket(client_fd, recv_buffer, bytes_left);
-        if (bytes_read == 0) {
+            ssize_t f_bytes_written = write(f_fd, buffer, bytes_read);
+            if (f_bytes_written < 0) {
+                perror("write");
+                break;
+            }
+
+            if (buffer[bytes_read - 1] == '\n') {
+                complete_packet = true;
+            }
+        } while (!complete_packet && running);
+
+        if (!complete_packet) {
             syslog(LOG_INFO, "Connection closed from %s", client_ip);
-            free(recv_buffer);
-            close(client_fd);
-            continue;
-        } else if (bytes_read < 0) {
-            free(recv_buffer);
-            close(client_fd);
-            continue;
-        } else {
-            // continue processing
-        }
-
-        FILE *f = fopen(TMP_FILE_PATH, "a");
-        if (f == NULL) {
-            perror("fopen");
-            free(recv_buffer);
             close(client_fd);
             continue;
         }
 
-        fwrite(recv_buffer, 1, bytes_read, f);
-        fclose(f);
+        ssize_t total_bytes_read = 0;
+        bool complete_file = false;
+        do {
+            if (lseek(f_fd, total_bytes_read, SEEK_SET) < 0) {
+                perror("lseek");
+                break;
+            }
 
-        // send response to client
-        ssize_t bytes_written = send(client_fd, recv_buffer, bytes_read, 0);
-        if (bytes_written < 0) {
-            perror("send");
-        }
+            ssize_t f_bytes_read = read(f_fd, buffer, RECV_DATA_MAX_LEN);
+            if (f_bytes_read < 0) {
+                perror("read");
+                break;
+            }
 
-        free(recv_buffer);
+            if (f_bytes_read < RECV_DATA_MAX_LEN) {
+                complete_file = true;
+            }
+
+            total_bytes_read += f_bytes_read;
+
+            printf("aesdsocket: sending %d bytes\n", (int)f_bytes_read);
+            ssize_t bytes_written = send(client_fd, buffer, f_bytes_read, 0);
+            if (bytes_written < 0) {
+                perror("send");
+                break;
+            }
+        } while (!complete_file && running);
+
+        syslog(LOG_INFO, "Connection closed from %s", client_ip);
         close(client_fd);
     }
 
+    printf("aesdsocket: exiting\n");
     closelog();
     close(socket_fd);
+    close(f_fd);
+    free(buffer);
+    if (unlink(TMP_FILE_PATH) < 0) {
+        perror("unlink");
+    }
 
     return 0;
 }
