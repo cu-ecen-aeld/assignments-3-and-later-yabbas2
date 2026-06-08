8381a023b8f9e0b87244e957dfc5bd6a8824147b
yabbas2 <yousef.abbas.2@outlook.com>
2026-06-08 08:17:22 -0500

    Fix buildroot build errors

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -39,51 +39,49 @@ MODULE_LICENSE("Dual BSD/GPL");
 static int aesd_open(struct inode *inode, struct file *filp)
 {
 	PDEBUG("open");
-	/**
-     * TODO: handle open
-     */
 	return 0;
 }
 
 static int aesd_release(struct inode *inode, struct file *filp)
 {
 	PDEBUG("release");
-	/**
-     * TODO: handle release
-     */
 	return 0;
 }
 
 static ssize_t aesd_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
 	ssize_t retval = 0;
-	PDEBUG("read %zu bytes with offset %lld", count, *f_pos);
-
-	int ret = mutex_lock_interruptible(&aesd_dev.mutex);
-    if (ret != 0) {
-        return ret;
-    }
-
+	int ret;
 	loff_t pos = *f_pos;
 	size_t pos_in_entry;
-	struct aesd_buffer_entry *entry =
-		aesd_circular_buffer_find_entry_offset_for_fpos(&aesd_dev.circ_buf, pos, &pos_in_entry);
-    if (entry != NULL) {
-        size_t bytes_left_in_entry = entry->size - pos_in_entry;
-        size_t bytes_to_read = min(bytes_left_in_entry, count);
+	struct aesd_buffer_entry *entry;
 
-        ssize_t not_copied = copy_to_user(buf, entry->buffptr + pos_in_entry, bytes_to_read);
-        if (not_copied == bytes_to_read) {
-            return -EFAULT;
-        }
-        bytes_to_read -= not_copied;
-        pos += bytes_to_read;
-        retval = bytes_to_read;
-    }
+	PDEBUG("read %zu bytes with offset %lld", count, *f_pos);
 
-    *f_pos = pos;
+	ret = mutex_lock_interruptible(&aesd_dev.mutex);
+	if (ret != 0) {
+		return ret;
+	}
 
-    mutex_unlock(&aesd_dev.mutex);
+	entry = aesd_circular_buffer_find_entry_offset_for_fpos(&aesd_dev.circ_buf, pos, &pos_in_entry);
+	if (entry != NULL) {
+		size_t bytes_left_in_entry = entry->size - pos_in_entry;
+		ssize_t not_copied;
+		size_t bytes_to_read;
+
+		bytes_to_read = min(bytes_left_in_entry, count);
+		not_copied = copy_to_user(buf, entry->buffptr + pos_in_entry, bytes_to_read);
+		if (not_copied == bytes_to_read) {
+			return -EFAULT;
+		}
+		bytes_to_read -= not_copied;
+		pos += bytes_to_read;
+		retval = bytes_to_read;
+	}
+
+	*f_pos = pos;
+
+	mutex_unlock(&aesd_dev.mutex);
 
 	return retval;
 }
@@ -91,12 +89,15 @@ static ssize_t aesd_read(struct file *filp, char __user *buf, size_t count, loff
 static ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
 {
 	ssize_t retval;
+	int ret;
+	ssize_t not_copied;
+
 	PDEBUG("write %zu bytes with offset %lld", count, *f_pos);
 
-	int ret = mutex_lock_interruptible(&aesd_dev.mutex);
-    if (ret != 0) {
-        return ret;
-    }
+	ret = mutex_lock_interruptible(&aesd_dev.mutex);
+	if (ret != 0) {
+		return ret;
+	}
 
 	wrtbuf.buffptr = (wrtbuf.buffptr == NULL) ? kmalloc(count, GFP_KERNEL) :
 						    krealloc(wrtbuf.buffptr, wrtbuf.size + count, GFP_KERNEL);
@@ -104,26 +105,26 @@ static ssize_t aesd_write(struct file *filp, const char __user *buf, size_t coun
 		return -ENOMEM;
 	}
 
-	ssize_t not_copied = copy_from_user(wrtbuf.buffptr + wrtbuf.size, buf, count);
+	not_copied = copy_from_user(wrtbuf.buffptr + wrtbuf.size, buf, count);
 	if (not_copied == count) {
 		return -EFAULT;
 	}
 	count -= not_copied;
 	wrtbuf.size += count;
-    retval = count;
+	retval = count;
 
 	if (wrtbuf.buffptr[wrtbuf.size - 1] == '\n') {
+		struct aesd_buffer_entry entry = { .buffptr = wrtbuf.buffptr, .size = wrtbuf.size };
 		if (aesd_circular_buffer_is_full(&aesd_dev.circ_buf)) {
 			struct aesd_buffer_entry *popped_entry = aesd_circular_buffer_pop_entry(&aesd_dev.circ_buf);
 			kfree(popped_entry->buffptr);
 		}
-		struct aesd_buffer_entry entry = { .buffptr = wrtbuf.buffptr, .size = wrtbuf.size };
 		aesd_circular_buffer_add_entry(&aesd_dev.circ_buf, &entry);
 		wrtbuf.size = 0;
 		wrtbuf.buffptr = NULL;
 	}
 
-    mutex_unlock(&aesd_dev.mutex);
+	mutex_unlock(&aesd_dev.mutex);
 
 	return retval;
 }
@@ -137,7 +138,7 @@ struct file_operations aesd_fops = {
 	.llseek = default_llseek,
 };
 
-static int aesd_dev_uevent(const struct device *dev, struct kobj_uevent_env *env)
+static int aesd_dev_uevent(struct device *dev, struct kobj_uevent_env *env)
 {
 	add_uevent_var(env, "DEVMODE=0666");
 	return 0;
@@ -154,7 +155,7 @@ static int __init aesd_init_module(void)
 	}
 	PDEBUG("aesd char driver registered with major %d", aesd_major);
 
-	aesd_class = class_create(DRV_NAME);
+	aesd_class = class_create(THIS_MODULE, DRV_NAME);
 	if (!aesd_class) {
 		status = -ENOMEM;
 		goto free_chrdev;
